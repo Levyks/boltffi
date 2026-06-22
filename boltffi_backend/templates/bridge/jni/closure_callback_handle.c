d7dd9976a10b6f2150e8e8a7f4c39930a342616b
Ali A. Hilal <ali94_200970@yahoo.com>
2026-06-22 09:16:37 +0200

    split jni closure handle templates

--- boltffi_backend/templates/bridge/jni/closure_callback_handle.c
+++ boltffi_backend/templates/bridge/jni/closure_callback_handle.c
@@ -1,77 +1,8 @@
 {%- for handle in closure_handles %}
 
-typedef struct {
-    {{ handle.call_field }};
-    void *context;
-    void (*release)(void *);
-} {{ handle.ty }};
-
-static {{ handle.ty }} *{{ handle.ref_ }}(jlong value) {
-    return value == 0 ? NULL : ({{ handle.ty }} *)(uintptr_t)value;
-}
-
-static void {{ handle.release }}(jlong value) {
-    {{ handle.ty }} *closure = {{ handle.ref_ }}(value);
-    if (closure == NULL) {
-        return;
-    }
-    if (closure->release != NULL) {
-        closure->release(closure->context);
-    }
-    free(closure);
-}
-
-static jlong {{ handle.new }}(JNIEnv *env, {{ handle.call_field }}, void *context, void (*release)(void *)) {
-    {{ handle.ty }} *closure = ({{ handle.ty }} *)malloc(sizeof({{ handle.ty }}));
-    if (closure == NULL) {
-        if (release != NULL) {
-            release(context);
-        }
-        boltffi_jni_throw_runtime(env, "failed to allocate BoltFFI closure handle");
-        return 0;
-    }
-    closure->call = call;
-    closure->context = context;
-    closure->release = release;
-    return (jlong)(uintptr_t)closure;
-}
-
-JNIEXPORT void JNICALL {{ handle.release_symbol }}(JNIEnv *env, jclass cls, jlong value) {
-    (void)env;
-    (void)cls;
-    {{ handle.release }}(value);
-}
-
-JNIEXPORT {{ handle.jni_return_type }} JNICALL {{ handle.call_symbol }}(JNIEnv *env, jclass cls, jlong value{% for parameter in handle.closure.handle_parameters %}, {{ parameter.declaration }}{% endfor %}) {
-    (void)env;
-    (void)cls;
-    {{ handle.ty }} *closure = {{ handle.ref_ }}(value);
-    if (closure == NULL || closure->call == NULL) {
-{%- if handle.closure.returns_void %}
-        return;
-{%- else %}
-        return {{ handle.failure_value }};
-{%- endif %}
-    }
-{% include "bridge/jni/closure/handle_buffer_declarations.c" %}
-{% include "bridge/jni/closure/handle_byte_arrays.c" %}
-{% include "bridge/jni/closure/handle_vector_declarations.c" %}
-{% include "bridge/jni/closure/handle_direct_vectors.c" %}
-{%- if handle.closure.returns_void %}
-    closure->call(closure->context{% if handle.closure.has_rust_arguments %}, {{ handle.closure.rust_arguments }}{% endif %});
-{% include "bridge/jni/closure/handle_cleanup.c" %}
-{%- else %}
-    {{ handle.closure.c_return_type }} result = closure->call(closure->context{% if handle.closure.has_rust_arguments %}, {{ handle.closure.rust_arguments }}{% endif %});
-{% include "bridge/jni/closure/handle_cleanup.c" %}
-{%- if handle.closure.returns_bytes %}
-    return boltffi_jni_buffer_to_byte_array(env, result);
-{%- else if handle.closure.returns_record %}
-    return boltffi_jni_record_to_byte_array(env, &result, (uintptr_t)sizeof(result));
-{%- else if handle.closure.returns_callback_handle %}
-    return boltffi_jni_callback_handle_new_owned(env, result);
-{%- else %}
-    return ({{ handle.jni_return_type }})result;
-{%- endif %}
-{%- endif %}
-}
+{% include "bridge/jni/closure_callback_handle/storage.c" %}
+{% include "bridge/jni/closure_callback_handle/pointer.c" %}
+{% include "bridge/jni/closure_callback_handle/allocation.c" %}
+{% include "bridge/jni/closure_callback_handle/release.c" %}
+{% include "bridge/jni/closure_callback_handle/call.c" %}
 {%- endfor %}
